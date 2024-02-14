51608480245b4cc9044b9e3fb8b2ce7da0da5853
harjot-debut <harjot.singh@debutinfotech.com>
2024-02-14 14:54:05 +0530

    added vault readme

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# integra-hashicop-vault
+
+### SETUP VAULT 
+
+- Clone the Hashicop vault repository at home directory :-
+`cd $HOME && git clone https://github.com/harjot-debut/integra-hashicop-vault.git`
+
+- Move to cloned repo and Install hashicop vault using command :-  
+`cd $HOME/integra-hashicop-vault && ./install-vault.sh`
+
+- To check installed version , use command :-
+`vault version`
+
+- Inside this hashicop directory enter the IP address of the machine in the file **vault-config.json** in json field `'vaultIP`':-
+
+- Setup and start vault container using command :-
+`./setup-vault.sh` 
+
+> NOTE:- SAVE THE Unseal key and Token that will be used for unsealing and login to vault in future.
+  
+
--- install-vault.sh
+++ install-vault.sh
@@ -12,8 +12,6 @@ trap 'handle_error $LINENO "$BASH_COMMAND"' ERR
 
 
 
-
-
 echo""
 echo""
 echo "============================================================"
@@ -22,36 +20,6 @@ echo "============================================================"
 echo""
 echo""
 
-
-
-# sudo apt update && sudo apt install gpg wget
-
-# wget -O- https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
-
-# gpg --no-default-keyring --keyring /usr/share/keyrings/hashicorp-archive-keyring.gpg --fingerprint
-
-# echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
-
-# sudo apt update && sudo apt install vault
-
-
-#set -x
-
-#sudo apt-get update && sudo apt-get upgrade -y
-#sudo apt install net-tools
-
-
-#wget -O- https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
-#echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
-#sudo apt update && sudo apt install vault
-
-#set +x
-
-
-#systemctl daemon-reload
-#systemctl start vault
-#systemctl enable vault
-#systemctl status vault
 
 
 
